ca7f3dd329919284e22087d1b369e94edcae47ce
sogun3 <sogun3@gmail.com>
2016-01-30 02:28:31 -0500

    newly added frame_finalize.py

--- POLLUTION/algorithms/algorithm.py
+++ POLLUTION/algorithms/algorithm.py
@@ -30,11 +30,14 @@ of pollutant.
 
 
 import pandas as pd
+from math import radians, cos, sin, asin, sqrt, atan2
 df_main = pd.read_pickle('/tmp/spark-141-ego-master/work/spark-a1e98bf0-c481-4cb4-8f46-908b3817bdcc/userFiles-eade2650-a194-4d9d-aaea-f4eff5f2b8dc/MyPickle')
 
 df_main['alt'] = df_main['alt'].convert_objects(convert_numeric=True)
 df_main['uwnd'] = df_main['uwnd'].convert_objects(convert_numeric=True)
 df_['vwnd'] = df_main['vwnd'].convert_objects(convert_numeric=True)
+#change NaN, US , keshoka county and 1020 to others
+#declare unique country names
 df['source_O3'] = ""
 df['source_NO2'] = ""
 df['source_SO2'] = ""
@@ -53,28 +56,45 @@ for date in date_array:
             label = 'source_'+ pollutant
             df.loc[my_list[index][1],label]= my_list[index][0]
 
-def matrix(df,date,pollutant):
-    df = df.dropna(subset=[pollutant],axis = 0)
-    total_rows = df.count()
 
-    A = np.empty([total_rows,total_rows])
-    B = np.empty([total_rows])
+#Calculate the coefficient for source Q to place with concentration C
+#d is the directio vector between source and receptor
+#u is the wind vector
+def matrix(df,year,month,pollutant):
+    df = df[df['year'] == year]
+    df = df[df['month'] == month]
+
+    df = df.reset_index()
+    total_rows = len(df)
+
+    A = np.zeros((total_rows,total_rows))
+    B = np.zeros(total_rows)
 
     #This for loop is used to populate the matrix A and B
     for i in range(0,total_rows): #c1, c2 ..., initial
-    final = [df.loc[i,'lon'],df.loc[i,'lat'],df.loc[i,'alt']]
+        final = [df.loc[i,'lon'],df.loc[i,'lat'],df.loc[i,'alt']]
+        B.itemset(i,df.loc[i,pollutant])
         for j in range(0,total_rows): #q1, q2 ...
             init = [df.loc[j,'lon'],df.loc[j,'lat'],df.loc[j,'alt']]
             coefficient = a(df.loc[j,'uwnd'],df.loc[j,'vwnd'],init[0],init[1], init[2],final[0],final[1],final[2])
             A.itemset((i,j),coefficient)
-            B.itemset(i,df.loc[i,pollutant])
 
     #solve the matrix
     X = np.linalg.solve(A,B)
-
+    print A
+    print X
+    print B
     #Save as percentages and save it in matrix A
+
+    for c in range(0,total_rows):
+        if (X[c] < 0):
+            X[c] = abs(X[c])
+        for d in range(0,total_rows):
+            if (A[c,d] < 0):
+                A[c,d] = abs(A[c,d])
+
     for m in range(0,total_rows):
-        A[m] = numpy.multiply(A[m],X)
+        A[m] = np.multiply(A[m],X)
         sum_m = np.sum(A[m])
         for n in range(0,total_rows):
             new_value = (A[m,n]/sum_m)*100
@@ -82,17 +102,24 @@ def matrix(df,date,pollutant):
 
     my_list = []
     #make a series with all the rows, state names and
-    for a in range(0,total_rows):
-        new_series = pd.series(data = A[a], index = df.state)
-        grouppy = new_series.groupby(groupby.index).sum()
+    for b in range(0,total_rows):
+        new_series = pd.Series(data = A[b], index = df.city)
+        grouppy = new_series.groupby(new_series.index).sum()
         grouppy_dict = grouppy.to_dict()
-        listty = [grouppy_dict,df.loc[a,'index']]
+        item_del = []
+        for item in grouppy_dict:
+            if (grouppy_dict[item] < 0.01):
+                item_del.append(item)
+        for deletes in item_del:
+            del grouppy_dict[deletes]
+        #sort the dictionary by state symbol
+        #if the number of symbols is less than 50
+        #cross check with state symbol list
+        #if one does not match the other
+        # add a tag to that particular location
+        listty = [grouppy_dict,df.loc[b,'index']]
         my_list.append(listty)
-
-    #X is the solution, i.e. the concentration of sources, of different places
-    #A is the coefficeints multiplying the sources, i.e. the weight of each source
     return my_list
-
 
 #Calculate the coefficient for source Q to place with concentration C
 #d is the directio vector between source and receptor
@@ -102,9 +129,14 @@ def a(u_x, u_y, init_lon, init_lat, init_z, final_lon, final_lat, final_z):
     #init[1] / final[1]: latitude
     #init[2] / final[2]: altitude
 
-    #convert lat lon to cartesian coordinates
-    dx = ((final_lon-init_lon)*40000*cos((final_lat+init_lat)*pi/360)/360)*1000
-    dy = ((init_lat-final_lat)*40000/360)*1000
+
+    if ((init_lat == final_lat) and (init_lon == final_lon)):
+        haver = haversine(init_lon, init_lat, init_lon + 2.5, init_lat+2.5)
+    else:
+        #convert lat lon to cartesian coordinates
+        haver = haversine(init_lon, init_lat, final_lon, final_lat)
+    dx = haver['dist'] * cos(haver['bearing'])
+    dy = haver['dist'] * sin(haver['bearing'])
 
     #vector distance between initial and final location
     d = [dx, dy, (final_z - init_z)]
@@ -112,117 +144,118 @@ def a(u_x, u_y, init_lon, init_lat, init_z, final_lon, final_lat, final_z):
     dot_product = u_x*d[0] + u_y*d[1]
 
 
-    if (dot_product < 0):
-        return 0 #if the wind blows in opposite direction of the displacement vector, return 0
+    #if (dot_product < 0):
+        #return 0 #if the wind blows in opposite direction of the displacement vector, return 0
+    #else:
+    mag_u = np.sqrt(u_x**2 + u_y**2)
+    mag_d = np.sqrt(d[0]**2 + d[1]**2)
+    if ((mag_d == 0) or (dx == 0)):
+        sigma_y = 90 #assuming sigma is classified as D - Neutral
+        sigma_z = 5
+        y = 0
     else:
-        mag_u = sqrt(u_x**2 + u_y**2)
-        mag_d = sqrt(d[0]**2 + d[1]**2)
-        if (mag_d == 0):
-            sigma_y = 90 #assuming sigma is classified as D - Neutral
-            sigma_z = 5
-            y = 1
-        else:
-            theta = arccos(dot_product/(mag_u*mag_d))
-            y = d * sin(theta) # y displacement from place a to place b
-            #sigma y and z
-            downwind_x = d[0]
-            sigma_y = check_sigma_y(mag_u,downwind_x)
-            sigma_z = check_sigma_z(mag_u,downwind_x)
+        theta = np.arccos(dot_product/(mag_u*mag_d))
+        y = mag_d * np.sin(theta) # y displacement from place a to place b
+        #sigma y and z
+        downwind_x = d[0]
+        sigma_y = check_sigma_y(mag_u,abs(dx))
+        sigma_z = check_sigma_z(mag_u,abs(dx))
 
-        #other value constants
-        z = final_z
-        H = init_z
+    #other value constants
+    z = final_z
+    H = init_z
 
-        denominator = 2 * pi * sigma_y * sigma_z * u
-        numerator = exp(-0.5*((y**2/sigma_y**2)*(z-H)**2/sigma_z**2))
-        return numerator / denominator
+    denominator = 2*np.pi * sigma_y * sigma_z * mag_u
+    numerator = np.exp((-0.5*y**2/sigma_y**2)+(-0.5*(z-H)**2/sigma_z**2))
+    ans = (numerator / denominator)
+    return ans
 
 #compute sigma_z
 def check_sigma_z(wind_speed,downwind_x):
     wind = abs(wind_speed)
-    if (wind < 2){          #A-B
-        if(downwind_x<10000){
+    if (wind < 2):          #A-B
+        if(downwind_x<10000):
             return sigma((0.495+0.310)/2,downwind_x,(0.873+0.897)/2)
-        } else {
+        else:
             return sigma((0.606+0.523)/2,downwind_x,(0.851+0.840)/2)
-        }
-    }else if (wind <3){     #B
-        if(downwind_x<=10000){
+    elif (wind <3):     #B
+        if(downwind_x<=10000):
             return sigma(0.310,downwind_x,0.897)
-        } else{
+        else:
             return sigma(0.523,downwind_x,0.840)
-        }
-    }else if (wind < 5){    #C
-        if(downwind_x<=10000){
+    elif (wind < 5):   #C
+        if(downwind_x<=10000):
             return sigma(0.197,downwind_x,0.908)
-        } else{
+        else:
             return sigma(0.285,downwind_x,0.867)
-        }
-    }else if(wind < 6){     #C_D
-        if(downwind_x<=10000){
+    elif(wind < 6):     #C_D
+        if(downwind_x<=10000):
             return sigma((0.197+0.122)/2,downwind_x,(0.908+0.916)/2)
-        } else{
+        else:
             return sigma((0.285+0.193)/2,downwind_x,(0.867+0.865)/2)
-        }
-    }else{#D
-        if(downwind_x<=10000){
+    else:#D
+        if(downwind_x<=10000):
             return sigma(0.122,downwind_x,0.916)
-        }else{
+        else:
             return sigma(0.193,downwind_x,0.865)
-        }
-    }
 
 
 #compute sigma_y
 def check_sigma_y(wind_speed,downwind_x):
     wind = abs(wind_speed)
-    if (wind < 2){          #A-B
-        if(downwind_x<=100){
-            return 0
-        } else if (downwind_x<= 500){
+    if (wind < 2):          #A-B
+        if (downwind_x<= 500):
             return sigma((0.0383+1.393)/2,downwind_x,(1.281+0.9467)/2)
-        } else {
+        else:
             return sigma((0.0002539+0.04936)/2,downwind_x,(2.089+1.114)/2)
-        }
-    }else if (wind <3){     #B
-        if(downwind_x<=100){
-            return 0
-        } else if (downwind_x<= 500){
+    elif (wind <3):     #B
+        if (downwind_x<= 500):
             return sigma(1.393,downwind_x,0.9467)
-        } else{
+        else:
             return sigma(0.04936,downwind_x,1.114)
-        }
-    }else if (wind < 5){    #C
-        if(downwind_x<=100){
-            return 0
-        } else if (downwind_x<= 500){
+    elif (wind < 5):    #C
+        if (downwind_x<= 500):
             return sigma(0.1120,downwind_x,0.9100)
-        } else if (downwind_x<=5000){
+        elif (downwind_x<=5000):
             return sigma(0.1014,downwind_x,0.926)
-        }else{
+        else:
             return sigma(0.1154,downwind_x,0.9109)
-        }
-    }else if(wind < 6){     #C_D
-        if(downwind_x<=100){
-            return 0
-        } else if (downwind_x<= 500){
+    elif(wind < 6):     #C_D
+        if (downwind_x<= 500):
             return sigma((0.1120+0.0856)/2,downwind_x,(0.9100+0.8650)/2)
-        } else if (downwind_x<=5000){
+        elif (downwind_x<=5000):
             return sigma((0.1014+0.2591)/2,downwind_x,(0.926+0.6869)/2)
-        }else{
+        else:
             return sigma((0.1154+0.7368)/2,downwind_x,(0.9109+0.5642)/2)
-        }
-    }else{#D
-        if(downwind_x<=100){
-            return 0
-        } else if (downwind_x<= 500){
+    else:#D
+        if (downwind_x<= 500):
             return sigma(0.0856,downwind_x,0.8650)
-        } else if (downwind_x<=5000){
+        elif (downwind_x<=5000):
             return sigma(0.2591,downwind_x,0.6869)
-        }else{
+        else:
             return sigma(0.7368,downwind_x,0.5642)
-        }
-    }
 
 def sigma(constant, distance, power):
     return constant * (distance ** power)
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    bearing = atan2(sin(lon2-lon1)*cos(lat2), cos(lat1)*sin(lat2)*sin(lat1)*cos(lat2)*cos(lon2-lon1))
+    bearing = bearing + 2*np.pi
+    while (bearing > 2*np.pi):
+        bearing -= np.pi
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371
+    dist = c*r
+    return {'dist' : dist, 'bearing' : bearing}
